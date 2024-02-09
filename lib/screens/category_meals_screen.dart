ff0011c6b3be0d492e25fd9a497ff53104540499
hendrikvanderkaaden <hendrikvdkaaden@gmail.com>
2024-02-09 14:40:37 +0100

    Add img to detail page

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/dummy_data.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context)?.settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+    final theme = Theme.of(context);
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: theme.primaryColor,
+          title: Text(categoryTitle!),
+        ),
+        body: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return MealItem(title: categoryMeals[index].title, imageUrl: categoryMeals[index].imageUrl, duration: categoryMeals[index].duration, complexity: categoryMeals[index].complexity, affordability: categoryMeals[index].affordability);
+          },
+          itemCount: categoryMeals.length,
+        )
+    );
+  }
+}
