6e400d258fbb36dfbc668aab363dcee38fa1c254
hendrikvanderkaaden <hendrikvdkaaden@gmail.com>
2024-02-12 13:38:09 +0100

    Finishing detail page

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,9 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen()
       },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
     );
   }
 }
